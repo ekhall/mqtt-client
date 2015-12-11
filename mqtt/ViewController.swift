27ac36c39b36f166ca3be23c03667096b08a164c
E. Kevin Hall <hall@absinthe.org>
2015-12-11 14:28:08 -0500

    Initial Commit

---
+++ mqtt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mqtt
+//
+//  Created by E. Kevin Hall on 12/11/15.
+//  Copyright © 2015 E. Kevin Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
